088d7b69cf5833e6d63f9dc12bd08866f7b39ca3
Kiraiiiii <114433704+Kiraiiiii@users.noreply.github.com>
2022-10-24 20:39:42 +0530

    Add files via upload

---
+++ dec.js
@@ -0,0 +1,22 @@
+var b = document.querySelector("#movingbutton");
+b.addEventListener("mousemove",change);
+
+function change()
+{
+let i = Math.floor(Math.random()*500)+1;
+let j = Math.floor(Math.random()*500)+1;
+console.log('here' , i ,j , b.style.left , b.style.top);
+    b.style.left = i+'px';
+    b.style.top = j + "px";
+}
+
+b.addEventListener("click",change);
+
+function change()
+{
+let i = Math.floor(Math.random()*500)+1;
+let j = Math.floor(Math.random()*500)+1;
+console.log('here' , i ,j , b.style.left , b.style.top);
+    b.style.left = i+'px';
+    b.style.top = j + "px";
+}
